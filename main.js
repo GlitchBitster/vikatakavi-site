17303c755d708c55d6fbaa4673df0e8d90fdc1d5
GlitchBitster <sri.lingam15@gmail.com>
2026-03-05 14:23:05 +0530

    missing style changes added

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-import './style.css'
-
 document.addEventListener('DOMContentLoaded', () => {
   // Set current year in footer
   document.getElementById('year').textContent = new Date().getFullYear();
